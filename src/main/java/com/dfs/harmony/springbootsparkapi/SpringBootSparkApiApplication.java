6722c0328b2e3811ebdffeb721b4786cc3041bd9
eugene <eugene.goldberg117@gmail.com>
2017-12-29 14:10:05 -0600

    added commons-compiler dependency and log4j properties

--- src/main/java/com/dfs/harmony/springbootsparkapi/SpringBootSparkApiApplication.java
+++ src/main/java/com/dfs/harmony/springbootsparkapi/SpringBootSparkApiApplication.java
@@ -1,12 +1,37 @@
 package com.dfs.harmony.springbootsparkapi;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSparkApiApplication {
+public class SpringBootSparkApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSparkApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		SparkSession sparkSession = SparkSession
+
+				.builder()
+
+				.appName("SparkWithSpring")
+
+				.master("local")
+
+				.getOrCreate();
+
+		System.out.println("Spark Version: " + sparkSession.version());
+
+        Dataset<Row> df = sparkSession.read().json("./people.json");
+
+        df.show();
+
 	}
 }
